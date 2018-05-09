a83f64210722489457a0168dfaffa7b0948b3956
mikhail_miatselski <mikhail_miatselski@epam.com>
2018-05-09 20:28:13 +0300

    logging request body

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
 
 const options = {
-  key: fs.readFileSync('../private/cert/YOURPRIVATE.key'),
-  cert: fs.readFileSync('../private/cert/YOURPUBLIC.pem')
+  key: fs.readFileSync(path.join(__dirname, '../private/cert/YOURPRIVATE.key')),
+  cert: fs.readFileSync(path.join(__dirname, '../private/cert/YOURPUBLIC.pem'))
 };
 
 https.createServer(options, (req, res) => {
-  res.writeHead(200);
-  res.end('hello world');
-}).listen(8443);
+  if (req.method === 'POST') {
+    let body = '';
+
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on('end', () => {
+      console.log(body);
+
+      res.writeHead(200);
+      res.end();
+    });
+  } else {
+    res.writeHead(200);
+    res.end('hello world');
+  }
+}).listen(8443, () => {
+  console.log('listening on 8443');
+});
